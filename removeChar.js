537574cb0a3a069e768b28c23257f4023cdd1d6e
Diego Madariaga <diego.madariaga@orbisdata.cl>
2022-02-22 01:01:58 -0300

    phone y jest

---
+++ removeChar.js
@@ -0,0 +1,6 @@
+function removeChar(str) {
+    //You got this!
+    return str.slice(1, str.length - 1);
+}
+let result = removeChar("Hola");
+console.log(result);
--- topThreeWords.js
+++ topThreeWords.js
@@ -1,10 +1,13 @@
+//https://www.codewars.com/kata/51e056fe544cf36c410000fb/train/javascript
 function topThreeWords(text) {
+    text = text.replaceAll(",", "").replaceAll(".", "").replaceAll("!", "").replaceAll("?", "").replaceAll("\"", "").replaceAll(";", "").replaceAll(":", "").replaceAll("/", "");
+    console.log("texto",text);
     let words = text.split(/[^a-zA-Z0-9_]+/);
+    words = words.map(word => word.toLowerCase().replace(/[^a-z]/g, ""));
     let map = new Map();
     for (let word of words) {
-        word = word.toLowerCase();
         if (word.length === 0) {
-            return -1;
+            return[];
         }
         if (map.has(word)) {
             map.set(word, map.get(word) + 1);
@@ -12,8 +15,20 @@ function topThreeWords(text) {
             map.set(word, 1);
         }
     }
-    console.log(map);
+    if (map.size === 0) {
+        return [];
+    }
+    //sort map by value
+    let sortedMap = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+    
+    let result = [...sortedMap.keys()].slice(0, 3);
+    
+    return result;
 }
 
-topThreeWords("a a a  b  c c  d d d d  e e e e e");
-topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e");
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"));
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
+console.log(topThreeWords("a a c b b"));
+console.log(topThreeWords("  //wont won't won't "));
+console.log(topThreeWords("  , e   .. "));
+
